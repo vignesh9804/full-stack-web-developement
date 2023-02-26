806c52cd47948acee4406c97971b498588a4ec67
vignesh9804 <vignesh.karimikonda@gmail.com>
2023-02-26 16:45:20 +0530

    newly added once

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
-console.log(`
-vignesh
-25
-sathish 38
-`);
+const getData = async () => {
+    var result = await
+    fetch("https://jsonplaceholder.typicode.com/users");
+    var data = await result.json();
+    console.log(data);
+};
+getData();
