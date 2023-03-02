f633c03ab926138572591b191fa11c318932eb1d
vignesh9804 <vignesh.karimikonda@gmail.com>
2023-03-03 00:55:54 +0530

    added upto 2/3/23

--- javascript.js
+++ javascript.js
@@ -1,7 +1,27 @@
-const getData = async () => {
-    var result = await
-    fetch("https://jsonplaceholder.typicode.com/users");
-    var data = await result.json();
-    console.log(data);
+const taskContainer = document.querySelector(".task__container");
+console.log(taskContainer);
+const saveChanges = () => {
+    const taskData = {
+        id: `${Date.now()}`,
+        imageUrl: document.getElementById("imageurl").value,
+        taskTitle: document.getElementById("tasktitle").value,
+        taskType: document.getElementById("tasktype").value,
+        taskDescription: document.getElementById("taskdescription").value
+    };
+    const newCard = `
+    <div class="col-sm-12 col-md-6 col-lg-4">
+            <div class="card">
+              <div class="card-header d-flex justify-content-end gap-2">
+                <button type="button" class="btn btn-outline-success"><i class="fas fa-pencil-alt"></i></button>
+                <button type="button" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i></button>
+              </div>
+              <div class="card-body">
+                <img class="card-img-top" src=${taskData.imageUrl} alt="...">
+                <h5 class="card-title fw-bold text-primary mt-3">${taskData.taskTitle}</h5>
+                <p class="card-text">${taskData.taskDescription}</p>
+                <a href="#" class="btn btn-primary">${taskData.taskType}</a>
+              </div>
+            </div>
+          </div>`;
+    taskContainer.insertAdjacentHTML("beforeend",newCard);
 };
-getData();
